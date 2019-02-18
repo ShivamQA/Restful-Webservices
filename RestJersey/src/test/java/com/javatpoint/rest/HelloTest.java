e9a4dd369103318714fc3e76bf32a08e6b177df3
qainfotech <shivam.agarwal@qainfotech.com>
2019-02-18 11:56:07 +0530

    Test cases

--- RestJersey/src/test/java/com/javatpoint/rest/HelloTest.java
+++ RestJersey/src/test/java/com/javatpoint/rest/HelloTest.java
@@ -1,21 +1,50 @@
 package com.javatpoint.rest;
 
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
-import java.util.ArrayList;
+import javax.ws.rs.core.Response;
 
-import org.testng.*;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
-public class HelloTest
+
+
+public class HelloTest 
+
 {
-    @Test
-    public void TestArray()
-    {
-    	Hello ob = new Hello();
-    	
-       // Assert.assertEquals(ar2,ob.consecutive(ar1),"Test Case Failed");
-        
-    }
+@Test
+public void TestLogin() throws URISyntaxException {
+Hello ob = new Hello();
+Response rs = ob.login("ankit@gmail.com", "123456");
+boolean b = ob.b;
+
+Assert.assertEquals(true,b,"Failed");
+}
+@Test
+public void Testsignup() throws URISyntaxException
+{
+HelloTest obj = new HelloTest();
+Hello ob = new Hello();
+Response rs = ob.signup("Prakul","prakul@gmail.com","1234");
+obj.TestExample("prakul@gmail.com","1234");
+
+}
+
+
+public void TestExample(String email,String pwd) throws URISyntaxException {
+Hello ob = new Hello();
+Response rs = ob.login(email, pwd);
+boolean b = ob.b;
+
+Assert.assertEquals(true,b,"Failed");
+}
+
+
 }
